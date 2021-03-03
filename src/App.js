cdd5fc072dade5feb11c965331616a718e119002
Stanislau <pontty232@gmail.com>
2021-03-03 23:43:33 +0300

    fix: delete 2 comments

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import './App.css';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
-// import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="app-wrap">
       <Header />
-      {/* <Navbar /> */}
       <Main />
       <Footer />
     </div>
